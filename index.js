f0be9a5208f30cfde3c0451699f5e0f11e8e2183
overgrownFudge <overgrownfudge@gmail.com>
2022-07-16 17:31:22 -0400

    updated readme

--- index.js
+++ index.js
@@ -3,14 +3,9 @@ const displayPasswords = document.querySelector('.display-passwords-container')
 const displayPassword = document.querySelectorAll('.pw-display')
 const changePasswordLength = document.querySelector('.change-password-length')
 
-
-
 const characters = [
    'A', 'B', 'C', 'D', 'E', 'F', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '*', '&', '$', '#', '!', '?', '<', '>', '+'
 ]
-
-
-
 
 function generatePassword(num = 8) {
    const pwArr = []
